2a0c19997730c0a18bbae4701e4cccc4f47183dc
bocharsky-bw <bocharsky.bw@gmail.com>
2016-10-13 16:09:30 +0300

    Add Redis caching for some variables

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Snc\RedisBundle\SncRedisBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Video;
+use Predis\Connection\ConnectionException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -17,9 +18,31 @@ class DefaultController extends Controller
             ->findAll();
         $tags = $this->getUniqueOrderedTags($videos);
 
+        // Redis cache
+        try {
+            if ($this->getRedisClient()->exists('total_video_uploads_count')) {
+                $totalVideoUploadsCount = $this->getRedisClient()->get('total_video_uploads_count');
+            } else {
+                $totalVideoUploadsCount = $this->countTotalVideoUploads();
+                $this->getRedisClient()->set('total_video_uploads_count', $totalVideoUploadsCount, 'ex', 60); // 60s
+            }
+
+            if ($this->getRedisClient()->exists('total_video_views_count')) {
+                $totalVideoViewsCount = $this->getRedisClient()->get('total_video_views_count');
+            } else {
+                $totalVideoViewsCount = $this->countTotalVideoViews();
+                $this->getRedisClient()->set('total_video_views_count', $totalVideoViewsCount, 'ex', 60); // 60s
+            }
+        } catch (ConnectionException $e) {
+            $totalVideoUploadsCount = $this->countTotalVideoUploads();
+            $totalVideoViewsCount = $this->countTotalVideoViews();
+        }
+
         return $this->render('default/index.html.twig', [
             'videos' => $videos,
             'tags' => $tags,
+            'totalVideoUploadsCount' => $totalVideoUploadsCount,
+            'totalVideoViewsCount' => $totalVideoViewsCount,
         ]);
     }
 
@@ -46,10 +69,42 @@ class DefaultController extends Controller
     }
 
     /**
+     * @return int
+     */
+    private function countTotalVideoUploads()
+    {
+        sleep(1); // simulating a long computation: waiting for 1s
+
+        $fakedCount = intval(date('Hms') . rand(1, 9));
+
+        return $fakedCount;
+    }
+
+    /**
+     * @return int
+     */
+    private function countTotalVideoViews()
+    {
+        sleep(1); // simulating a long computation: waiting for 1s
+
+        $fakedCount = intval(date('Hms') . rand(1, 9)) * 111;
+
+        return $fakedCount;
+    }
+
+    /**
      * @return \Doctrine\Common\Persistence\ObjectRepository
      */
     private function getVideoRepository()
     {
         return $this->get('doctrine')->getRepository(Video::class);
+    }
+
+    /**
+     * @return object|\Predis\Client
+     */
+    private function getRedisClient()
+    {
+        return $this->get('snc_redis.default_client');
     }
 }
