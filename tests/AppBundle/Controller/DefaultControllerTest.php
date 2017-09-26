d33782b32c5d641c2d47a5c86f5d636ae0fcecbc
bocharsky-bw <bocharsky.bw@gmail.com>
2017-09-26 15:33:49 +0300

    [tuts-apply-diff] Applying existing diff file for step add-phpunit-tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexAction()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('MooTube Bovine Fitness', $crawler->filter('h1')->text());
+        $this->assertContains('Filter by Tag', $crawler->text());
+        $this->assertContains('Bovine Fitness Routines', $crawler->text());
+    }
+
+    public function testAboutAction()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/about');
+
+        $this->assertContains('Welcome to MooTube', $crawler->text());
+
+        $linkElement = $crawler->filter('.btn-explore');
+        $this->assertContains('Explore the content', $linkElement->text());
+        $this->assertEquals('/', $linkElement->attr('href'));
     }
 }
